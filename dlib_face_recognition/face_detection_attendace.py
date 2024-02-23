46656fc1f8a2e290a162edc917debcbbdd5ad475
bishnukdk <bishnukhadka442@gmail.com>
2024-02-23 20:03:37 +0545

    frame-rate feature added

--- dlib_face_recognition/face_detection_attendace.py
+++ dlib_face_recognition/face_detection_attendace.py
@@ -6,10 +6,68 @@ import time
 
 from utility.utility import *
 
-def main():
-    db = initialize_firestore()
-    # from PIL import ImageGrab
+# Global variables
+FRAME_RATE = 10  # Desired frame rate (frames per second)
+PREV_CAPTURE_TIME = 0
+CAPTURING = True
+IMAGE = None
+# Global lock for synchronizing access to process_image function
+process_image_lock = threading.Lock()
+attendance_status_dict = {}
 
+def process_image(encodeListKnown, classNames,db):
+    global attendance_status_dict
+    with process_image_lock:
+    # print(f"Thread {threading.current_thread().name}: captured at time: {prev_capture_time}")
+        imgS = cv2.resize(IMAGE, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        facesCurFrame = face_recognition.face_locations(imgS)
+        if (facesCurFrame):
+            encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+            for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+                matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+                faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+                # print(faceDis)
+                matchIndex = np.argmin(faceDis)
+
+                if matches[matchIndex]:
+                    name = classNames[matchIndex].upper()
+                    print(f"{name} detected.")
+                    y1, x2, y2, x1 = faceLoc
+                    y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                    cv2.rectangle(IMAGE, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.rectangle(IMAGE, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(IMAGE, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                    teacher_dict = get_teachers_dict()
+                    print(f"attendance_status_dict: {attendance_status_dict}")
+                    print(f"teacher_dict: {teacher_dict}")
+                    print(f"teacher_dict[name.lower()] : {teacher_dict[name.lower()]}")
+                    print(f"\n attendance_status_dict[teacher_dict[name.lower()]]: {attendance_status_dict[teacher_dict[name.lower()]]}")
+                    if(attendance_status_dict[teacher_dict[name.lower()]]==False):
+                        # if attencende for today is not done, then only call backend api
+                        print(f"attendenca for {name} is not done today. Recording Attendane...")
+                        # make attendance: calling firebase in different thread
+                        if(markAttendance(name,db)==True):
+                            attendance_status_dict[teacher_dict[name.lower()]]=True
+                            print(f"Attendence status updated: {switch_keys(attendance_status_dict, teacher_dict)}")
+                    else: 
+                        print(f"Attendance for {name} is already done for today")
+                        height, width, _ = IMAGE.shape
+                        cv2.putText(IMAGE, "Attendance already done.", (height//2-100, (width//2)+50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                        cv2.imshow("webcam", IMAGE)
+                        cv2.waitKey(1)
+                else:
+                    print(f"Thread {threading.current_thread().name}: Error: Failed to capture frame")
+
+def main():
+    global attendance_status_dict
+    db = initialize_firestore()
+    attendance_status_dict = get_attendance_dict_for_today(db)  
+    teacher_dict = get_teachers_dict()
+    attendence_status_today=switch_keys(attendance_status_dict, teacher_dict)
+    print(f"Attendance Status for today: {attendence_status_today}")
+    
     path = 'Training_images'
     images = []
     classNames = []
@@ -20,47 +78,36 @@ def main():
         images.append(curImg)
         classNames.append(os.path.splitext(cl)[0])
     print(classNames)
-    
-    #### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-    # def captureScreen(bbox=(300,300,690+300,530+300)):
-    #     capScr = np.array(ImageGrab.grab(bbox))
-    #     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-    #     return capScr
 
     encodeListKnown = findEncodings(images)
     print('Encoding Complete')
 
     cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print(f"Thread {threading.current_thread().name}: Error: Failed to open camera")
+        return
+    
+    global PREV_CAPTURE_TIME, FRAME_RATE, IMAGE
 
     while True:
-        time.sleep(0.3)
-        success, img = cap.read()
-        # img = captureScreen()
-        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-
-        facesCurFrame = face_recognition.face_locations(imgS)
-        encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
-
-        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-            # print(faceDis)
-            matchIndex = np.argmin(faceDis)
-
-            if matches[matchIndex]:
-                name = classNames[matchIndex].upper()
-                print(f"{name} detected.")
-                y1, x2, y2, x1 = faceLoc
-                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-                markAttendance(name, db)
-
-        cv2.imshow('Webcam', img)
-        cv2.waitKey(1)
-
+        time_elapsed = time.time() - PREV_CAPTURE_TIME
+        
+        if time_elapsed > 1. / FRAME_RATE:
+            PREV_CAPTURE_TIME = time.time()
+            
+            success, img = cap.read()
+            if success:
+                IMAGE = img
+                process_image(encodeListKnown=encodeListKnown, 
+                              classNames=classNames, 
+                              db=db)
+            cv2.imshow("webcam", IMAGE)
+        # Check for 'q' key pressed to exit the loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            CAPTURING = False
+            break
+    cap.release()
+    cv2.destroyAllWindows()
     
 if __name__ == "__main__":
     main()
--- dlib_face_recognition/utility/utility.py
+++ dlib_face_recognition/utility/utility.py
@@ -1,3 +1,4 @@
+import copy
 import face_recognition
 import cv2
 from datetime import datetime
@@ -5,7 +6,9 @@ import os
 import json
 import firebase_admin
 from firebase_admin import firestore, credentials
-
+import threading
+# Global lock for synchronizing access to process_image function
+attendance_lock = threading.Lock()
 
 def initialize_firestore():
     # Initialize Firestore
@@ -39,81 +42,86 @@ def read_json_file(file_path):
 
 
 def get_date_and_count_from_attendance_data(attendence_doc):
+    # print("get_date_and_count_form_attendence_data() called.")
     # Extract the keys
     year = list(attendence_doc.keys())[0]
     month = list(attendence_doc[year].keys())[0]
-    print(f"day list: {attendence_doc[year][month].keys()}")
-    day = list(attendence_doc[year][month].keys())[-1]
-    count = list(attendence_doc[year][month].keys())[0]
+    count = attendence_doc[year][month]['count']
+    day = list(attendence_doc[year][month].keys())
+    day.remove('count')
+    day.sort(reverse=True)
+    print(f" inside get_date_and_count_from_attendence=> day_list_reverse: {day}")
+    day= day[0]
 
     # Construct the desired string
     date_string = f"{year}-{month}-{day}"
+    print(f"get_date_and_count_from_attendance_data: {date_string}")
     return date_string, count
 
 
 def markAttendance(name, db):
-    current_time = datetime.now().strftime("%H:%M:%S")
-    """
-    Record attendance in Firestore.
-    """
-    # Get today's date
-    # TODO: need the data for time
-    today = datetime.now().strftime("%Y-%m-%d")
-    temp_list_attendence = {}
-    today = datetime.now().strftime("%Y-%m-%d")
-    split_today = today.split(sep="-")
-    print(split_today[0])
-    attendance_data = {
-            split_today[0]: {
-                split_today[1]: {
-                    'count': 1,
-                    split_today[2]: {
-                        'check_in_time': current_time
-                    }
-                }
-            }
-        }
-    teacher_ids = get_teacher_ids()
-    user_id = teacher_ids[name.lower()]
-    # print(f"{user_id} for {name}")
-    try:  
-        # Get the attendance record for today
-        # attendance_ref = db.collection('teacher_attendance').document(user_id).collection('last_recorded_date').document('record')
-        attendance_ref = db.collection('teacher_attendance').document(user_id).collection('year').document('record')
-        attendance_doc = attendance_ref.get()
-        attendance_doc_dict = attendance_doc.to_dict()
-        # DEBUG: 
-        print(f"GOT attendence doc: {attendance_doc_dict}")
-
-        if  not attendance_doc.exists:  
-            attendance_ref.set(attendance_data)
-            print(f"Attendence record SET for {name} for date: {today}")
-        else:
-            date_from_att_doc, month_att_count = get_date_and_count_from_attendance_data(attendance_doc_dict)    
-            print(f"date form att doc: {date_from_att_doc}, today: {today}")
-            if date_from_att_doc != today:    
-                # Update attendance count if no record exists for today or if it's a new day)
-                attendance_data = {
-                    split_today[0]: {
-                        split_today[1]: {
-                            'count': month_att_count+1,
-                            split_today[2]: {
-                                'check_in_time': current_time
-                            }
+    print("markAttendance function called. ")
+    with attendance_lock:
+        current_time = datetime.now().strftime("%H:%M:%S")
+        print(f"Thread {threading.current_thread().name}: ")
+        """
+        Record attendance in Firestore.
+        """
+        # Get today's date
+        # TODO: need the data for time
+        today = datetime.now().strftime("%Y-%m-%d")
+        split_today = today.split(sep="-")
+        print(split_today[0])
+        attendance_data = {
+                split_today[0]: {
+                    split_today[1]: {
+                        'count': 1,
+                        split_today[2]: {
+                            'check_in_time': current_time
                         }
                     }
                 }
-                attendance_ref.update(attendance_data)
-                print(f"Attendance recorded UPDATE for {name} for date:{today}")
+            }
+        teacher_ids = get_teachers_dict()
+        user_id = teacher_ids[name.lower()]
+        # print(f"{user_id} for {name}")
+        try:  
+            # Get the attendance record for today
+            # attendance_ref = db.collection('teacher_attendance').document(user_id).collection('last_recorded_date').document('record')
+            attendance_ref = db.collection('teacher_attendance').document(user_id).collection('year').document('record')
+            attendance_doc = attendance_ref.get()
+            attendance_doc_dict = attendance_doc.to_dict()
+            temp_dict = copy.deepcopy(attendance_doc_dict)
+            # DEBUG: 
+            print(f"GOT attendence doc: {attendance_doc_dict}")
+
+            if  not attendance_doc.exists:  
+                attendance_ref.set(attendance_data)
+                print(f"Attendence record SET for {name} for date: {today}")
             else:
-                # Attendance record for today exists
-                # this means that the person has already done attendence today. 
-                print(f"{name}'s attendence has already been recorded for today.")
-    except Exception as e:
-        print("Error recording attendance:", e)
+                date_from_att_doc, month_att_count = get_date_and_count_from_attendance_data(attendance_doc_dict)    
+                print(f"date form att doc: {date_from_att_doc}, today: {today}")
+                # also the date_from_att_doc should be in correct format.
+                print(f"are_all_numbers: are_all_numbers(split_today)")
+                if (all_elements_convertible_to_int(split_today) and date_from_att_doc != today):
+                    attendance_data = attendance_doc_dict[split_today[0]][split_today[1]]
+                    attendance_data.update({'count': month_att_count+1, split_today[2]: {'check_in_time': "current_time"}})
+                    month_attendance_data = attendance_doc_dict[split_today[0]]
+                    month_attendance_data.update({split_today[1]: attendance_data})
+                    temp_dict[split_today[0]] = month_attendance_data
+                    print(temp_dict)
+                    attendance_ref.update(temp_dict)
+                    print(f"Attendance recorded UPDATE for {name} for date:{today}")
+                else:
+                    # Attendance record for today exists
+                    # this means that the person has already done attendence today. 
+                    print(f"Either last attended date = Today OR date could not be retrieved .")
+        except Exception as e:
+            print("Error recording attendance:", e)
+            return False
+    return True
 
-
-def get_teacher_ids():
+def get_teachers_dict():
     """
     outputs dict of teachers name(key) and id(value)
     """
@@ -121,3 +129,53 @@ def get_teacher_ids():
     teachers_dict = read_json_file(os.path.join(teachers_json_filename))
     return teachers_dict
 
+def get_attendance_dict_for_today(db):
+    attendance_ref = db.collection('teacher_attendance')
+    teacher_id_dict = get_teachers_dict()
+    today = datetime.now().strftime("%Y-%m-%d")
+    split_today = today.split(sep="-")
+
+    attendance_status_dict = {}
+    for i,teacher_id in enumerate(teacher_id_dict.values()):
+        attendance_doc = attendance_ref.document(teacher_id).collection('year').document('record').get()
+        attendance_doc_dict = attendance_doc.to_dict()
+        print(f"{teacher_id}: docs = {attendance_doc_dict}")
+        date, count = get_date_and_count_from_attendance_data(attendence_doc=attendance_doc_dict)
+
+        # temp_dict = copy.deepcopy(attendance_doc_dict)
+        # attendance_data = attendance_doc_dict[split_today[0]][split_today[1]]
+        # attendance_data.update({'count': count+1, split_today[2]: {'check_in_time': "current_time"}})
+        # month_attendance_data = attendance_doc_dict[split_today[0]]
+        # month_attendance_data.update({split_today[1]: attendance_data})
+        # temp_dict[split_today[0]] = month_attendance_data
+        # print(f"temp_dict: {temp_dict}")
+
+        print()
+        if(date == today ):
+            attendance_status_dict[teacher_id] = True
+        else:
+            attendance_status_dict[teacher_id] = False
+    return attendance_status_dict
+
+def switch_keys(dict1, dict2):
+
+    # Create a new dictionary to store the attendance status with teacher names as keys
+    dict3 = {}
+
+    # Iterate through the attendance status dictionary
+    for teacher_name, teacher_key in dict2.items():
+        # Use the teacher key to get the attendance status
+        temp = dict1.get(teacher_key)
+        # Add the attendance status to the new dictionary with teacher name as key
+        dict3[teacher_name] = temp
+
+    return dict3
+
+def are_all_numbers(lst):
+    for element in lst:
+        if not isinstance(element, (int, float)):
+            return False
+    return True
+
+def all_elements_convertible_to_int(lst):
+    return all(isinstance(elem, int) or (isinstance(elem, str) and elem.isdigit()) for elem in lst)
